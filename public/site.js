d5ba05dfa8a57ac409fb06603b4eaed100efc03a
David Steller <dstellar@student.cvtc.edu>
2023-09-10 15:37:12 -0500

    troubleshooting dark-mode version2

--- public/site.js
+++ public/site.js
@@ -1,21 +1,19 @@
-document.ready(function () {
-  'nav'.on('click', function () {
-    'nav'.toggle()
-  })
 
-  '#style-toggle'.on('click', function () {
-    if ('body'.hasClass('dark')) {
-      'body'.removeClass('dark')
-      'h2, h3'.removeClass('dark')
-      'header'.removeClass('dark-accent')
-      'nav'.removeClass('dark-accent')
-    } else {
-      'body'.addClass('dark')
-      'h2, h3'.addClass('dark')
-      'header'.addClass('dark-accent')
-      'nav'.addClass('dark-accent')
-    }
-  })
-})
+/*
+let toggleBtn = document.getElementById('style-toggle')
 
+window.onload = function () {
+    const styleToggle = document.getElementById('style-toggle')
+    styleToggle.addEventListener('click',
+      function (event) {
+        event.preventDefault()
+        window.alert('Toggling dark and light mode not yet implemented.')
+      }
+    )
+  }
+  */
 
+  function darkMode() {
+    let element = document.body
+    element.classList.toggle("dark")
+  }

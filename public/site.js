23e3dbbab011da605bbddc08a7e692ed98f4ea7b
dsteller <144038703+dsteller@users.noreply.github.com>
2023-09-11 18:39:21 -0500

    Update site.js dark mode function working

--- public/site.js
+++ public/site.js
@@ -1,19 +1,12 @@
-
-/*
-let toggleBtn = document.getElementById('style-toggle')
-
-window.onload = function () {
-    const styleToggle = document.getElementById('style-toggle')
-    styleToggle.addEventListener('click',
-      function (event) {
-        event.preventDefault()
-        window.alert('Toggling dark and light mode not yet implemented.')
-      }
-    )
-  }
-  */
-
 function darkMode() {
-  let element = document.body
-  element.classList.toggle('dark')
+  // toggle to dark mode
+  const dkButton = document.querySelector('nav > button');
+  document.body.classList.toggle('dark');
+
+  // change dark mode button
+  if (body.classList.contains('dark')) {
+    dkButton.innerHTML = '☀️';
+  } else {
+    dkButton.innerHTML = '🌙';
+  }
 }
